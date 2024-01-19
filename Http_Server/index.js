6f18a9676254861a75a0aba948cdb58b34f548e2
hamzabilalaydin <hamzaba61@gmail.com>
2024-01-19 16:37:06 +0300

    first server created

--- Http_Server/index.js
+++ Http_Server/index.js
@@ -1,3 +1,63 @@
+// const http = require("http");
+
+// const PORT = 3000;
+
+// const server = http.createServer();
+
+// const friends = [
+//   {
+//     id: 0,
+//     name: "Nikola Tesla",
+//   },
+//   {
+//     id: 1,
+//     name: "Sir Isaac Newton",
+//   },
+//   {
+//     id: 2,
+//     name: "Albert Einstein",
+//   },
+// ];
+
+// server.on("request", (req, res) => {
+//   const items = req.url.split("/");
+//   if (req.method === "POST" && items[1] === "friends") {
+//     req.on((data) => {
+//       const friend = data.toString();
+//       console.log("Request: ", friend);
+//       friends.push(JSON.parse(friend));
+//     });
+//     req.pipe();
+//   } else if (req.method === "GET" && items[1] === "friends") {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "application/json");
+//     if (items.length === 3) {
+//       const friendsIndex = Number(items[2]);
+//       res.end(JSON.stringify(friends[friendsIndex]));
+//     } else {
+//       res.end(JSON.stringify(friends));
+//     }
+//   } else if (req.method === "GET" && items[1] === "messages") {
+//     res.setHeader("Content-Type", "text/html");
+//     res.write("<html>");
+//     res.write("<body>");
+//     res.write("<ul>");
+//     res.write("<li>Hello Isaac!</li>");
+//     res.write("<li>What are your thoughts on astronomy?</li>");
+//     res.write("</ul>");
+//     res.write("</body>");
+//     res.write("</html>");
+//     res.end(); //should end!!!!!!!
+//   } else {
+//     res.statusCode = 404;
+//     res.end();
+//   }
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Listening on port ${PORT}...`);
+// });
+
 const http = require("http");
 
 const PORT = 3000;
@@ -19,22 +79,27 @@ const friends = [
   },
 ];
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
-
 server.on("request", (req, res) => {
   const items = req.url.split("/");
-  if (items[1] === "friends") {
+  // /friends/2 => ['', 'friends', '2']
+  // /friends/
+  if (req.method === "POST" && items[1] === "friends") {
+    req.on("data", (data) => {
+      const friend = data.toString();
+      console.log("Request:", friend);
+      friends.push(JSON.parse(friend));
+    });
+    req.pipe(res);
+  } else if (req.method === "GET" && items[1] === "friends") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     if (items.length === 3) {
-      const friendsIndex = Number(items[2]);
-      res.end(JSON.stringify(friends[friendsIndex]));
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
     } else {
       res.end(JSON.stringify(friends));
     }
-  } else if (items[1] === "messages") {
+  } else if (req.method === "GET" && items[1] === "messages") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<body>");
@@ -44,9 +109,13 @@ server.on("request", (req, res) => {
     res.write("</ul>");
     res.write("</body>");
     res.write("</html>");
-    res.end(); //should end!!!!!!!
+    res.end();
   } else {
     res.statusCode = 404;
     res.end();
   }
 });
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+}); //127.0.0.1 => localhost
